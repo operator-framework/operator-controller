4fae06376734ffafca90f84ff0261ee7c4e9052f
timflannagan <timflannagan@gmail.com>
2022-06-23 10:55:36 -0400

    Refactor the platform operators controller
    The platform operator's controller now interfaces with deppy's
    Resolution API directly, and removes any CatalogSource handlement and
    applying rukpak BundleInstance's directly.
    
    Update the controller to only accomodate a cluster singleton resource,
    which specifies the desired set of packages that need to be managed on
    the cluster.
    
    Signed-off-by: timflannagan <timflannagan@gmail.com>

--- controllers/platformoperator_controller.go
+++ controllers/platformoperator_controller.go
@@ -1,61 +1,41 @@
-/*
-Copyright 2022.
-
-Licensed under the Apache License, Version 2.0 (the "License");
-you may not use this file except in compliance with the License.
-You may obtain a copy of the License at
-
-    http://www.apache.org/licenses/LICENSE-2.0
-
-Unless required by applicable law or agreed to in writing, software
-distributed under the License is distributed on an "AS IS" BASIS,
-WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-See the License for the specific language governing permissions and
-limitations under the License.
-*/
-
 package controllers
 
 import (
 	"context"
 
-	operatorsv1alpha1 "github.com/operator-framework/api/pkg/operators/v1alpha1"
-	rukpakv1alpha1 "github.com/operator-framework/rukpak/api/v1alpha1"
-	"k8s.io/apimachinery/pkg/api/meta"
-	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/runtime"
+	"k8s.io/apimachinery/pkg/types"
 	ctrl "sigs.k8s.io/controller-runtime"
 	"sigs.k8s.io/controller-runtime/pkg/client"
-	"sigs.k8s.io/controller-runtime/pkg/handler"
 	logr "sigs.k8s.io/controller-runtime/pkg/log"
-	"sigs.k8s.io/controller-runtime/pkg/source"
 
+	"github.com/operator-framework/deppy/api/v1alpha1"
 	platformv1alpha1 "github.com/timflannagan/platform-operators/api/v1alpha1"
-	"github.com/timflannagan/platform-operators/internal/applier"
-	"github.com/timflannagan/platform-operators/internal/sourcer"
-	"github.com/timflannagan/platform-operators/internal/util"
 )
 
 // PlatformOperatorReconciler reconciles a PlatformOperator object
 type PlatformOperatorReconciler struct {
 	client.Client
-	Sourcer sourcer.Sourcer
-	Applier applier.Applier
-	Scheme  *runtime.Scheme
+	Scheme *runtime.Scheme
 }
+
+const (
+	packageConstraintType = "olm.RequirePackage"
+	packageValueKey       = "package"
+	platformResolution    = "platform"
+	platformSingletonName = "cluster"
+)
 
 //+kubebuilder:rbac:groups=platform.openshift.io,resources=platformoperators,verbs=get;list;watch;create;update;patch;delete
 //+kubebuilder:rbac:groups=platform.openshift.io,resources=platformoperators/status,verbs=get;update;patch
 //+kubebuilder:rbac:groups=platform.openshift.io,resources=platformoperators/finalizers,verbs=update
-//+kubebuilder:rbac:groups=operators.coreos.com,resources=catalogsources,verbs=get;list;watch
-//+kubebuilder:rbac:groups=core.rukpak.io,resources=bundleinstances,verbs=get;list;watch;create;update;patch;delete
-//+kubebuilder:rbac:groups=core.rukpak.io,resources=bundles,verbs=get;list;watch;create;update;patch;delete
+//+kubebuilder:rbac:groups=core.deppy.io,resources=resolutions,verbs=get;list;watch;update;patch
 
 // Reconcile is part of the main kubernetes reconciliation loop which aims to
 // move the current state of the cluster closer to the desired state.
 //
 // For more details, check Reconcile and its Result here:
-// - https://pkg.go.dev/sigs.k8s.io/controller-runtime@v0.10.0/pkg/reconcile
+// - https://pkg.go.dev/sigs.k8s.io/controller-runtime@v0.11.2/pkg/reconcile
 func (r *PlatformOperatorReconciler) Reconcile(ctx context.Context, req ctrl.Request) (ctrl.Result, error) {
 	log := logr.FromContext(ctx)
 	log.Info("reconciling request", "req", req.NamespacedName)
@@ -66,6 +46,10 @@ func (r *PlatformOperatorReconciler) Reconcile(ctx context.Context, req ctrl.Req
 	if err := r.Get(ctx, req.NamespacedName, po); err != nil {
 		return ctrl.Result{}, client.IgnoreNotFound(err)
 	}
+	// filter out any non-cluster singletons
+	if po.GetName() != platformSingletonName {
+		return ctrl.Result{}, nil
+	}
 	defer func() {
 		po := po.DeepCopy()
 		po.ObjectMeta.ManagedFields = nil
@@ -74,46 +58,35 @@ func (r *PlatformOperatorReconciler) Reconcile(ctx context.Context, req ctrl.Req
 		}
 	}()
 
-	desiredBundle, err := r.Sourcer.Source(ctx, po)
-	if err != nil {
-		meta.SetStatusCondition(&po.Status.Conditions, metav1.Condition{
-			Type:    platformv1alpha1.TypeSourced,
-			Status:  metav1.ConditionUnknown,
-			Reason:  platformv1alpha1.ReasonSourceFailed,
-			Message: err.Error(),
-		})
+	res := &v1alpha1.Resolution{}
+	if err := r.Get(ctx, types.NamespacedName{Name: platformResolution}, res); err != nil {
+		log.Error(err, "failed to find the resolution resource")
 		return ctrl.Result{}, err
 	}
-	meta.SetStatusCondition(&po.Status.Conditions, metav1.Condition{
-		Type:    platformv1alpha1.TypeSourced,
-		Status:  metav1.ConditionTrue,
-		Reason:  platformv1alpha1.ReasonSourceSuccessful,
-		Message: "Successfully sourced the desired olm.bundle content",
-	})
 
-	if err := r.Applier.Apply(ctx, po, desiredBundle); err != nil {
-		meta.SetStatusCondition(&po.Status.Conditions, metav1.Condition{
-			Type:    platformv1alpha1.TypeApplied,
-			Status:  metav1.ConditionUnknown,
-			Reason:  platformv1alpha1.ReasonApplyFailed,
-			Message: err.Error(),
-		})
-		return ctrl.Result{}, err
+	var (
+		desiredPackages []v1alpha1.Constraint
+	)
+	for _, name := range po.Spec.Packages {
+		desiredPackages = append(desiredPackages, newPackageRequirement(name))
 	}
-	meta.SetStatusCondition(&po.Status.Conditions, metav1.Condition{
-		Type:    platformv1alpha1.TypeApplied,
-		Status:  metav1.ConditionTrue,
-		Reason:  platformv1alpha1.ReasonApplySuccessful,
-		Message: "Successfully applied the desired olm.bundle content",
-	})
-	return ctrl.Result{}, nil
+	// TODO: sort these packages to ensure determinism
+	res.Spec.Constraints = desiredPackages
+	return ctrl.Result{}, r.Client.Update(ctx, res)
+}
+
+func newPackageRequirement(packageName string) v1alpha1.Constraint {
+	return v1alpha1.Constraint{
+		Type: packageConstraintType,
+		Value: map[string]string{
+			packageValueKey: packageName,
+		},
+	}
 }
 
 // SetupWithManager sets up the controller with the Manager.
 func (r *PlatformOperatorReconciler) SetupWithManager(mgr ctrl.Manager) error {
 	return ctrl.NewControllerManagedBy(mgr).
 		For(&platformv1alpha1.PlatformOperator{}).
-		Watches(&source.Kind{Type: &operatorsv1alpha1.CatalogSource{}}, handler.EnqueueRequestsFromMapFunc(util.RequeuePlatformOperators(mgr.GetClient()))).
-		Watches(&source.Kind{Type: &rukpakv1alpha1.BundleInstance{}}, handler.EnqueueRequestsFromMapFunc(util.RequeueBundleInstance(mgr.GetClient()))).
 		Complete(r)
 }
--- test/e2e/platform_operators_test.go
+++ test/e2e/platform_operators_test.go
@@ -2,17 +2,12 @@ package e2e
 
 import (
 	"context"
-	"fmt"
 	"path/filepath"
 
 	. "github.com/onsi/ginkgo/v2"
 	. "github.com/onsi/gomega"
-	rukpakv1alpha1 "github.com/operator-framework/rukpak/api/v1alpha1"
 	corev1 "k8s.io/api/core/v1"
-	"k8s.io/apimachinery/pkg/api/meta"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
-	"k8s.io/apimachinery/pkg/types"
-	"sigs.k8s.io/controller-runtime/pkg/client"
 
 	platformv1alpha1 "github.com/timflannagan/platform-operators/api/v1alpha1"
 )
@@ -61,39 +56,13 @@ var _ = Describe("platform operators controller", func() {
 						GenerateName: "prometheus-operator",
 					},
 					Spec: platformv1alpha1.PlatformOperatorSpec{
-						PackageName: "prometheus-operator",
+						Packages: []string{"prometheus-operator"},
 					},
 				}
 				Expect(c.Create(ctx, po)).To(BeNil())
 			})
 			AfterEach(func() {
 				Expect(c.Delete(ctx, po)).To(BeNil())
-			})
-			It("should initially select the v0.1.0 package", func() {
-				Eventually(func() bool {
-					bi := &rukpakv1alpha1.BundleInstance{}
-					if err := c.Get(ctx, types.NamespacedName{Name: po.GetName()}, bi); err != nil {
-						return false
-					}
-					return bi.Spec.Template.Spec.Source.Image.Ref == "quay.io/operatorhubio/prometheus:v0.47.0"
-				}).Should(BeTrue())
-			})
-			When("the catalog has been updated and a new olm.bundle version is available", func() {
-				BeforeEach(func() {
-					updatedProvider, err := NewFileBasedFiledBasedCatalogProvider(filepath.Join(dataBaseDir, "prometheus.v0.2.0.yaml"))
-					Expect(err).To(BeNil())
-
-					Expect(catalog.UpdateCatalog(ctx, updatedProvider)).To(BeNil())
-				})
-				It("should result in the v0.2.0 package being installed", func() {
-					Eventually(func() bool {
-						bi := &rukpakv1alpha1.BundleInstance{}
-						if err := c.Get(ctx, types.NamespacedName{Name: po.GetName()}, bi); err != nil {
-							return false
-						}
-						return bi.Spec.Template.Spec.Source.Image.Ref == "quay.io/operatorhubio/prometheus:v0.47.0--20220413T184225"
-					}).Should(BeTrue())
-				})
 			})
 		})
 
@@ -107,69 +76,13 @@ var _ = Describe("platform operators controller", func() {
 						GenerateName: "prometheus-operator",
 					},
 					Spec: platformv1alpha1.PlatformOperatorSpec{
-						PackageName: "prometheus-operator",
+						Packages: []string{"prometheus-operator"},
 					},
 				}
 				Expect(c.Create(ctx, po)).To(BeNil())
 			})
 			AfterEach(func() {
 				Expect(c.Delete(ctx, po)).To(BeNil())
-			})
-			It("should generate a bundleinstance with a metadata.Name that matches the platformoperator's metadata.Name", func() {
-				Eventually(func() error {
-					bi := &rukpakv1alpha1.BundleInstance{}
-					return c.Get(ctx, types.NamespacedName{Name: po.GetName()}, bi)
-				}).Should(Succeed())
-			})
-			It("should generate a bundleinstance that contains the different unique provisioner ID", func() {
-				Eventually(func() bool {
-					bi := &rukpakv1alpha1.BundleInstance{}
-					if err := c.Get(ctx, types.NamespacedName{Name: po.GetName()}, bi); err != nil {
-						return false
-					}
-					return bi.Spec.Template.Spec.ProvisionerClassName != bi.Spec.ProvisionerClassName
-				}).Should(BeTrue())
-			})
-			It("should choose the highest olm.bundle semver available in the catalog", func() {
-				Eventually(func() bool {
-					bi := &rukpakv1alpha1.BundleInstance{}
-					if err := c.Get(ctx, types.NamespacedName{Name: po.GetName()}, bi); err != nil {
-						return false
-					}
-					return bi.Spec.Template.Spec.Source.Image.Ref == "quay.io/operatorhubio/prometheus:v0.47.0"
-				}).Should(BeTrue())
-			})
-			It("should result in a successful application", func() {
-				Eventually(func() (*metav1.Condition, error) {
-					if err := c.Get(ctx, client.ObjectKeyFromObject(po), po); err != nil {
-						return nil, err
-					}
-					return meta.FindStatusCondition(po.Status.Conditions, platformv1alpha1.TypeApplied), nil
-				}).Should(And(
-					Not(BeNil()),
-					WithTransform(func(c *metav1.Condition) string { return c.Type }, Equal(platformv1alpha1.TypeApplied)),
-					WithTransform(func(c *metav1.Condition) metav1.ConditionStatus { return c.Status }, Equal(metav1.ConditionTrue)),
-					WithTransform(func(c *metav1.Condition) string { return c.Reason }, Equal(platformv1alpha1.ReasonApplySuccessful)),
-					WithTransform(func(c *metav1.Condition) string { return c.Message }, ContainSubstring("Successfully applied the desired olm.bundle content")),
-				))
-			})
-			It("should result in a successful installation", func() {
-				Eventually(func() (*metav1.Condition, error) {
-					bi := &rukpakv1alpha1.BundleInstance{}
-					if err := c.Get(ctx, types.NamespacedName{Name: po.GetName()}, bi); err != nil {
-						return nil, err
-					}
-					if bi.Status.InstalledBundleName == "" {
-						return nil, fmt.Errorf("waiting for bundle name to be populated")
-					}
-					return meta.FindStatusCondition(bi.Status.Conditions, rukpakv1alpha1.TypeInstalled), nil
-				}).Should(And(
-					Not(BeNil()),
-					WithTransform(func(c *metav1.Condition) string { return c.Type }, Equal(rukpakv1alpha1.TypeInstalled)),
-					WithTransform(func(c *metav1.Condition) metav1.ConditionStatus { return c.Status }, Equal(metav1.ConditionTrue)),
-					WithTransform(func(c *metav1.Condition) string { return c.Reason }, Equal(rukpakv1alpha1.ReasonInstallationSucceeded)),
-					WithTransform(func(c *metav1.Condition) string { return c.Message }, ContainSubstring("instantiated bundle")),
-				))
 			})
 		})
 	})
