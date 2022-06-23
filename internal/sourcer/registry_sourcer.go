d21a5fc9fc2893d10701e5c55e7e1043d0d2b307
timflannagan <timflannagan@gmail.com>
2022-06-23 11:04:38 -0400

    internal: Refactor source library and decouple platform operators
    Signed-off-by: timflannagan <timflannagan@gmail.com>

--- internal/sourcer/registry_sourcer.go
+++ internal/sourcer/registry_sourcer.go
@@ -2,18 +2,17 @@ package sourcer
 
 import (
 	"context"
+	"errors"
 	"fmt"
 
 	operatorsv1alpha1 "github.com/operator-framework/api/pkg/operators/v1alpha1"
 	registryClient "github.com/operator-framework/operator-registry/pkg/client"
 	utilerror "k8s.io/apimachinery/pkg/util/errors"
 	"sigs.k8s.io/controller-runtime/pkg/client"
-
-	platformv1alpha1 "github.com/timflannagan/platform-operators/api/v1alpha1"
 )
 
-const (
-	channelName = "4.12"
+var (
+	ErrNoCandidates = errors.New("failed to find any catalog candidates")
 )
 
 type catalogSource struct {
@@ -26,7 +25,7 @@ func NewCatalogSourceHandler(c client.Client) Sourcer {
 	}
 }
 
-func (cs catalogSource) Source(ctx context.Context, po *platformv1alpha1.PlatformOperator) (*Bundle, error) {
+func (cs catalogSource) Source(ctx context.Context) (*Bundle, error) {
 	css := &operatorsv1alpha1.CatalogSourceList{}
 	if err := cs.List(ctx, css); err != nil {
 		return nil, err
@@ -36,22 +35,21 @@ func (cs catalogSource) Source(ctx context.Context, po *platformv1alpha1.Platfor
 	}
 	sources := sources(css.Items)
 
-	candidates, err := sources.Filter(byConnectionReadiness).GetCandidates(ctx, po)
+	candidates, err := sources.Filter(byConnectionReadiness).GetCandidates(ctx)
 	if err != nil {
 		return nil, err
 	}
 	if len(candidates) == 0 {
-		return nil, fmt.Errorf("failed to find candidate olm.bundles from the %s package", po.Spec.PackageName)
+		return nil, ErrNoCandidates
 	}
 	latestBundle, err := candidates.Latest()
 	if err != nil {
 		return nil, err
 	}
-
 	return latestBundle, nil
 }
 
-func (s sources) GetCandidates(ctx context.Context, po *platformv1alpha1.PlatformOperator) (bundles, error) {
+func (s sources) GetCandidates(ctx context.Context) (bundles, error) {
 	var (
 		errors     []error
 		candidates bundles
@@ -72,9 +70,6 @@ func (s sources) GetCandidates(ctx context.Context, po *platformv1alpha1.Platfor
 			continue
 		}
 		for b := it.Next(); b != nil; b = it.Next() {
-			if b.PackageName != po.Spec.PackageName || b.ChannelName != channelName {
-				continue
-			}
 			candidates = append(candidates, Bundle{
 				Version:  b.GetVersion(),
 				Image:    b.GetBundlePath(),
