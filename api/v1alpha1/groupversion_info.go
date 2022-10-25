19c43c64a82886d6dd091e0e7eda1bc8b41f664e
timflannagan <timflannagan@gmail.com>
2022-10-25 16:41:15 -0400

    *: Rename the Operator GVK
    Signed-off-by: timflannagan <timflannagan@gmail.com>

--- api/v1alpha1/groupversion_info.go
+++ api/v1alpha1/groupversion_info.go
@@ -16,7 +16,7 @@ limitations under the License.
 
 // Package v1alpha1 contains API Schema definitions for the operator v1alpha1 API group
 // +kubebuilder:object:generate=true
-// +groupName=operator.operator.io
+// +groupName=core.olm.io
 package v1alpha1
 
 import (
@@ -26,7 +26,7 @@ import (
 
 var (
 	// GroupVersion is group version used to register these objects
-	GroupVersion = schema.GroupVersion{Group: "operator.operator.io", Version: "v1alpha1"}
+	GroupVersion = schema.GroupVersion{Group: "core.olm.io", Version: "v1alpha1"}
 
 	// SchemeBuilder is used to add go types to the GroupVersionKind scheme
 	SchemeBuilder = &scheme.Builder{GroupVersion: GroupVersion}
