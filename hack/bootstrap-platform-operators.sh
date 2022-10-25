8b1d32c77360a3743b39880efe3ec150ab417c5d
timflannagan <timflannagan@gmail.com>
2022-10-25 14:56:53 -0400

    hack: Ensure the PlatformOperator API exists on the cluster
    Signed-off-by: timflannagan <timflannagan@gmail.com>

--- hack/bootstrap-platform-operators.sh
+++ hack/bootstrap-platform-operators.sh
@@ -5,10 +5,18 @@ set -o nounset
 set -o errexit
 
 FILE=packages.json
+: "${KUBECONFIG:?}"
 
 function registry_get_packages() {
     if [[ ! -f $FILE ]]; then
         grpcurl -plaintext  localhost:50051 api.Registry/ListPackages | jq '.name' > "$FILE"
+    fi
+}
+
+function platform_operator_API_exists() {
+    if ! kubectl get crd platformoperators.platform.openshift.io; then
+        echo "You need to install the PlatformOperators API"
+        exit 1
     fi
 }
 
@@ -22,13 +30,14 @@ cat <<EOF | kubectl apply -f -
 apiVersion: platform.openshift.io/v1alpha1
 kind: PlatformOperator
 metadata:
-name: $p
+  name: $p
 spec:
-package:
+  package:
     name: $p
 EOF
 done <"$file"
 }
 
 registry_get_packages
+platform_operator_API_exists
 create_platform_operators "$FILE"
