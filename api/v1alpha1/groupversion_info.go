0d64f314bec837ba580d57d869471993b7a4b5f8
timflannagan <timflannagan@gmail.com>
2022-10-25 16:17:50 -0400

    api/v1alpha1: Run go fmt on groupversion_info.go
    Signed-off-by: timflannagan <timflannagan@gmail.com>

--- api/v1alpha1/groupversion_info.go
+++ api/v1alpha1/groupversion_info.go
@@ -15,8 +15,8 @@ limitations under the License.
 */
 
 // Package v1alpha1 contains API Schema definitions for the operator v1alpha1 API group
-//+kubebuilder:object:generate=true
-//+groupName=operator.operator.io
+// +kubebuilder:object:generate=true
+// +groupName=operator.operator.io
 package v1alpha1
 
 import (
