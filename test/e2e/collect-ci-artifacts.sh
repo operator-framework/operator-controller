cd72ba608b8010c69c87e422ffcc47ab32411b32
timflannagan <timflannagan@gmail.com>
2022-11-08 16:06:45 -0500

    test/e2e: Remove downstream-only APIs when collecting testing artifacts
    Signed-off-by: timflannagan <timflannagan@gmail.com>

--- test/e2e/collect-ci-artifacts.sh
+++ test/e2e/collect-ci-artifacts.sh
@@ -6,18 +6,9 @@ set -o errexit
 
 : "${KUBECONFIG:?}"
 : "${ARTIFACT_DIR:?}"
-: "${KUBECTL:=oc}"
+: "${KUBECTL:=kubectl}"
 
 function ensure_kubectl() {
-    # Check whether we're running in downstream CI environments as the location for the
-    # "oc" binary is located at "/cli/oc" path. This is problematic as the /cli directory
-    # doesn't exist in the $PATH environment variable, which causes issues when running
-    # this script via the exec.Command Golang function.
-    if [[ "$OPENSHIFT_CI" == "true" ]]; then
-        echo "Detected the e2e suite is being run in CI environment. Adding the /cli to \$PATH"
-        export PATH=$PATH:/cli
-    fi
-
     if ! which ${KUBECTL} &> /dev/null; then
         echo "cannot find kubectl binary in \$PATH"
         exit 1
@@ -26,10 +17,6 @@ function ensure_kubectl() {
 
 function collect_artifacts() {
     commands=()
-    # TODO(tflannag): Figure out a graceful way to collect downstream
-    # only artifacts.
-    # commands+=("get co platform-operators-aggregated -o yaml")
-    # commands+=("get platformoperators -o yaml")
     commands+=("get operators.core.olm.io -o yaml")
     commands+=("get bundledeployments -o yaml")
     commands+=("get bundles -o yaml")
@@ -46,6 +33,7 @@ function collect_artifacts() {
 function main() {
     echo "Using the ${KUBECTL} kubectl binary"
     echo "Using the ${ARTIFACT_DIR} output directory"
+    echo "Using the ${KUBECONFIG} kubeconfig"
     mkdir -p "${ARTIFACT_DIR}"
 
     ensure_kubectl
