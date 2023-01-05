47485a7e8e678ff7f922321d014e8ae1adeef352
Daniel Franz <dfranz@redhat.com>
2023-01-05 15:12:52 -0800

    Adding e2e make target, makefile adjustments, and e2e test framework (#81)
    * Adding e2e make target, makefile adjustments, and e2e test framework
    
    * addressing comments
    
    * added e2e github action and added pending tag to test
    
    * back-out scoping change
    
    * disable -j flag for make
    
    Signed-off-by: dtfranz <dfranz@redhat.com>

---
+++ test/e2e/e2e_suite_test.go
@@ -0,0 +1,38 @@
+package e2e
+
+import (
+	"testing"
+	"time"
+
+	. "github.com/onsi/ginkgo/v2"
+	. "github.com/onsi/gomega"
+	"k8s.io/apimachinery/pkg/runtime"
+	"k8s.io/client-go/rest"
+	ctrl "sigs.k8s.io/controller-runtime"
+	"sigs.k8s.io/controller-runtime/pkg/client"
+
+	operatorv1alpha1 "github.com/operator-framework/operator-controller/api/v1alpha1"
+)
+
+var (
+	cfg *rest.Config
+	c   client.Client
+)
+
+func TestE2E(t *testing.T) {
+	RegisterFailHandler(Fail)
+	SetDefaultEventuallyTimeout(1 * time.Minute)
+	SetDefaultEventuallyPollingInterval(1 * time.Second)
+	RunSpecs(t, "E2E Suite")
+}
+
+var _ = BeforeSuite(func() {
+	cfg = ctrl.GetConfigOrDie()
+
+	scheme := runtime.NewScheme()
+	err := operatorv1alpha1.AddToScheme(scheme)
+	Expect(err).To(Not(HaveOccurred()))
+
+	c, err = client.New(cfg, client.Options{Scheme: scheme})
+	Expect(err).To(Not(HaveOccurred()))
+})
