fffa59bd6f91add526b4235a064dd1b34829545e
Per Goncalves da Silva <perdasilva@redhat.com>
2023-01-25 11:16:22 +0100

    [resolution pt.1] bundle entity (#95)
    * update vendor
    * add bundle entity
    * add entity source predicates
    * add entity sort functions
    
    Signed-off-by: perdasilva <perdasilva@redhat.com>

--- go.mod
+++ go.mod
@@ -3,9 +3,11 @@ module github.com/operator-framework/operator-controller
 go 1.19
 
 require (
+	github.com/blang/semver/v4 v4.0.0
 	github.com/onsi/ginkgo/v2 v2.3.1
 	github.com/onsi/gomega v1.22.1
 	github.com/operator-framework/deppy v0.0.0-20230102161649-36fa82370999
+	github.com/operator-framework/operator-registry v1.26.2
 	k8s.io/apimachinery v0.25.0
 	k8s.io/client-go v0.25.0
 	sigs.k8s.io/controller-runtime v0.13.1
@@ -40,7 +42,7 @@ require (
 	github.com/google/gnostic v0.5.7-v3refs // indirect
 	github.com/google/go-cmp v0.5.8 // indirect
 	github.com/google/gofuzz v1.1.0 // indirect
-	github.com/google/uuid v1.1.2 // indirect
+	github.com/google/uuid v1.2.0 // indirect
 	github.com/imdario/mergo v0.3.12 // indirect
 	github.com/josharian/intern v1.0.0 // indirect
 	github.com/json-iterator/go v1.1.12 // indirect
@@ -58,7 +60,7 @@ require (
 	go.uber.org/atomic v1.7.0 // indirect
 	go.uber.org/multierr v1.6.0 // indirect
 	go.uber.org/zap v1.21.0 // indirect
-	golang.org/x/crypto v0.0.0-20220315160706-3147a52a75dd // indirect
+	golang.org/x/crypto v0.0.0-20220408190544-5352b0902921 // indirect
 	golang.org/x/net v0.0.0-20220722155237-a158d28d115b // indirect
 	golang.org/x/oauth2 v0.0.0-20211104180415-d3ed0bb246c8 // indirect
 	golang.org/x/sys v0.0.0-20220722155257-8c9f86f7a55f // indirect
---
+++ internal/resolution/variable_sources/utils/predicates/predicates.go
@@ -0,0 +1,58 @@
+package predicates
+
+import (
+	"github.com/blang/semver/v4"
+	"github.com/operator-framework/deppy/pkg/deppy/input"
+
+	olmentity "github.com/operator-framework/operator-controller/internal/resolution/variable_sources/entity"
+)
+
+func WithPackageName(packageName string) input.Predicate {
+	return func(entity *input.Entity) bool {
+		bundleEntity := olmentity.NewBundleEntity(entity)
+		name, err := bundleEntity.PackageName()
+		if err != nil {
+			return false
+		}
+		return name == packageName
+	}
+}
+
+func InSemverRange(semverRange semver.Range) input.Predicate {
+	return func(entity *input.Entity) bool {
+		bundleEntity := olmentity.NewBundleEntity(entity)
+		bundleVersion, err := bundleEntity.Version()
+		if err != nil {
+			return false
+		}
+		return semverRange(*bundleVersion)
+	}
+}
+
+func InChannel(channelName string) input.Predicate {
+	return func(entity *input.Entity) bool {
+		bundleEntity := olmentity.NewBundleEntity(entity)
+		bundleChannel, err := bundleEntity.ChannelName()
+		if err != nil {
+			return false
+		}
+		return channelName == bundleChannel
+	}
+}
+
+func ProvidesGVK(gvk *olmentity.GVK) input.Predicate {
+	return func(entity *input.Entity) bool {
+		bundleEntity := olmentity.NewBundleEntity(entity)
+		providedGVKs, err := bundleEntity.ProvidedGVKs()
+		if err != nil {
+			return false
+		}
+		for i := 0; i < len(providedGVKs); i++ {
+			providedGVK := &providedGVKs[i]
+			if providedGVK.String() == gvk.String() {
+				return true
+			}
+		}
+		return false
+	}
+}
