d30f161864b644b7d36afcb877dc51e7c0c0475d
Jordan Keister <jordan@nimblewidget.com>
2023-08-30 17:15:20 -0500

    update demo based on API changes (#154)
    Signed-off-by: Jordan Keister <jordan@nimblewidget.com>

--- hack/scripts/generate-asciidemo.sh
+++ hack/scripts/generate-asciidemo.sh
@@ -1,20 +1,18 @@
 #!/usr/bin/env bash
-## run against a local copy of https://github.com/grokspawn/asciidemo-tools
-#  export TMPDIR=~/tmp/   # if linux, since docker doesn't like remodeling /tmp/ pathing
-#  ./asciidemo-tools/generate-gif.sh ./generate-asciidemo.sh demo.gif
+# run against a local copy of https://github.com/grokspawn/asciidemo-tools
+#  ../asciidemo-tools/generate-gif.sh hack/scripts/generate-asciidemo.sh docs/demo.gif
 
 SCRIPTPATH="$( cd -- "$(dirname "$0")" > /dev/null 2>&1 ; pwd -P )"
 
-ASCIIDEMO_TOOLS=./asciidemo-tools/demo-functions.sh
+ASCIIDEMO_TOOLS=../asciidemo-tools/demo-functions.sh
 REPO="https://github.com/operator-framework/catalogd"
 
 . $ASCIIDEMO_TOOLS
 
 function run() {
-    if [ -e ./catalogd ] ; then echo "ERROR:  ./catalogd exists.  Please delete before running this script."; exit 1; fi
     typeline -x  "# Welcome to the catalogd demo"
-    typeline "git clone $REPO"
-    typeline "cd catalogd"
+    typeline "make kind-cluster-cleanup"
+    typeline "kind delete cluster"
     typeline "make kind-cluster"
     typeline "kubectl cluster-info --context kind-catalogd"
     sleep 10
@@ -27,7 +25,7 @@ function run() {
     typeline "kubectl apply -f config/samples/core_v1alpha1_catalog.yaml" # or other
     typeline "kubectl get catalog -A" # shows catalog-sample
     typeline -x "# waiting for catalog to report ready status"
-    typeline "kubectl wait --for=condition=Unpacked catalog/operatorhubio --timeout=1h"
+    typeline "time kubectl wait --for=condition=Unpacked catalog/operatorhubio --timeout=1h"
     # inspect packages, and then details on one package
     typeline -x "# check what 'packages' are available in this catalog and then inspect the content of one of the packages"
     typeline "kubectl get catalogmetadata -l schema=olm.package"
