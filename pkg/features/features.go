b8f36268517dc39280fe964d131a6e3ff7ac9335
Mikalai Radchuk <509198+m1kola@users.noreply.github.com>
2023-10-06 14:12:28 +0100

    Add ForceSemverUpgradeConstraints feature gate (#438)
    Signed-off-by: Mikalai Radchuk <mradchuk@redhat.com>

--- pkg/features/features.go
+++ pkg/features/features.go
@@ -6,13 +6,17 @@ import (
 )
 
 const (
-// Add new feature gates constants (strings)
-// Ex: SomeFeature featuregate.Feature = "SomeFeature"
+	// Add new feature gates constants (strings)
+	// Ex: SomeFeature featuregate.Feature = "SomeFeature"
+
+	ForceSemverUpgradeConstraints featuregate.Feature = "ForceSemverUpgradeConstraints"
 )
 
 var operatorControllerFeatureGates = map[featuregate.Feature]featuregate.FeatureSpec{
 	// Add new feature gate definitions
 	// Ex: SomeFeature: {...}
+
+	ForceSemverUpgradeConstraints: {Default: false, PreRelease: featuregate.Alpha},
 }
 
 var OperatorControllerFeatureGate featuregate.MutableFeatureGate = featuregate.NewFeatureGate()
