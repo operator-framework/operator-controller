8ecced71e24fcd54e91c448fa4da02c0ada93292
Mikalai Radchuk <mradchuk@redhat.com>
2023-10-31 15:48:26 +0000

    Move bundle variable code into a func
    Signed-off-by: Mikalai Radchuk <mradchuk@redhat.com>

--- internal/resolution/variablesources/bundle.go
+++ internal/resolution/variablesources/bundle.go
@@ -30,9 +30,8 @@ func NewBundlesAndDepsVariableSource(allBundles []*catalogmetadata.Bundle, input
 }
 
 func (b *BundlesAndDepsVariableSource) GetVariables(ctx context.Context) ([]deppy.Variable, error) {
-	var variables []deppy.Variable
+	variables := []deppy.Variable{}
 
-	// extract required package variables
 	for _, variableSource := range b.variableSources {
 		inputVariables, err := variableSource.GetVariables(ctx)
 		if err != nil {
@@ -41,18 +40,43 @@ func (b *BundlesAndDepsVariableSource) GetVariables(ctx context.Context) ([]depp
 		variables = append(variables, inputVariables...)
 	}
 
-	// create bundle queue for dependency resolution
-	var bundleQueue []*catalogmetadata.Bundle
+	requiredPackages := []*olmvariables.RequiredPackageVariable{}
+	installedPackages := []*olmvariables.InstalledPackageVariable{}
 	for _, variable := range variables {
 		switch v := variable.(type) {
 		case *olmvariables.RequiredPackageVariable:
-			bundleQueue = append(bundleQueue, v.Bundles()...)
+			requiredPackages = append(requiredPackages, v)
 		case *olmvariables.InstalledPackageVariable:
-			bundleQueue = append(bundleQueue, v.Bundles()...)
+			installedPackages = append(installedPackages, v)
 		}
 	}
 
+	bundles, err := MakeBundleVariables(b.allBundles, requiredPackages, installedPackages)
+	if err != nil {
+		return nil, err
+	}
+
+	for _, v := range bundles {
+		variables = append(variables, v)
+	}
+	return variables, nil
+}
+
+func MakeBundleVariables(
+	allBundles []*catalogmetadata.Bundle,
+	requiredPackages []*olmvariables.RequiredPackageVariable,
+	installedPackages []*olmvariables.InstalledPackageVariable,
+) ([]*olmvariables.BundleVariable, error) {
+	var bundleQueue []*catalogmetadata.Bundle
+	for _, variable := range requiredPackages {
+		bundleQueue = append(bundleQueue, variable.Bundles()...)
+	}
+	for _, variable := range installedPackages {
+		bundleQueue = append(bundleQueue, variable.Bundles()...)
+	}
+
 	// build bundle and dependency variables
+	var result []*olmvariables.BundleVariable
 	visited := sets.Set[deppy.Identifier]{}
 	for len(bundleQueue) > 0 {
 		// pop head of queue
@@ -68,32 +92,34 @@ func (b *BundlesAndDepsVariableSource) GetVariables(ctx context.Context) ([]depp
 		visited.Insert(id)
 
 		// get bundle dependencies
-		dependencies, err := b.filterBundleDependencies(b.allBundles, head)
+		dependencies, err := filterBundleDependencies(allBundles, head)
 		if err != nil {
-			return nil, fmt.Errorf("could not determine dependencies for bundle with id '%s': %w", id, err)
+			return nil, fmt.Errorf("could not determine dependencies for bundle with id %q: %w", id, err)
 		}
 
 		// add bundle dependencies to queue for processing
 		bundleQueue = append(bundleQueue, dependencies...)
 
 		// create variable
-		variables = append(variables, olmvariables.NewBundleVariable(head, dependencies))
+		result = append(result, olmvariables.NewBundleVariable(head, dependencies))
 	}
 
-	return variables, nil
+	return result, nil
 }
 
-func (b *BundlesAndDepsVariableSource) filterBundleDependencies(allBundles []*catalogmetadata.Bundle, bundle *catalogmetadata.Bundle) ([]*catalogmetadata.Bundle, error) {
+func filterBundleDependencies(allBundles []*catalogmetadata.Bundle, bundle *catalogmetadata.Bundle) ([]*catalogmetadata.Bundle, error) {
 	var dependencies []*catalogmetadata.Bundle
 	added := sets.Set[deppy.Identifier]{}
 
 	// gather required package dependencies
-	// todo(perdasilva): disambiguate between not found and actual errors
 	requiredPackages, _ := bundle.RequiredPackages()
 	for _, requiredPackage := range requiredPackages {
-		packageDependencyBundles := catalogfilter.Filter(allBundles, catalogfilter.And(catalogfilter.WithPackageName(requiredPackage.PackageName), catalogfilter.InBlangSemverRange(requiredPackage.SemverRange)))
+		packageDependencyBundles := catalogfilter.Filter(allBundles, catalogfilter.And(
+			catalogfilter.WithPackageName(requiredPackage.PackageName),
+			catalogfilter.InBlangSemverRange(requiredPackage.SemverRange),
+		))
 		if len(packageDependencyBundles) == 0 {
-			return nil, fmt.Errorf("could not find package dependencies for bundle '%s'", bundle.Name)
+			return nil, fmt.Errorf("could not find package dependencies for bundle %q", bundle.Name)
 		}
 		for i := 0; i < len(packageDependencyBundles); i++ {
 			bundle := packageDependencyBundles[i]
