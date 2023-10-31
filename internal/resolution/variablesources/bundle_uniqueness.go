2ac8ff8fd70069b7968c3c4549709a593ef9328e
Mikalai Radchuk <mradchuk@redhat.com>
2023-10-31 14:53:16 +0000

    Move bundle uniqueness code into a func
    Signed-off-by: Mikalai Radchuk <mradchuk@redhat.com>

--- internal/resolution/variablesources/bundle_uniqueness.go
+++ internal/resolution/variablesources/bundle_uniqueness.go
@@ -4,15 +4,53 @@ import (
 	"context"
 	"fmt"
 
+	"k8s.io/apimachinery/pkg/util/sets"
+
 	"github.com/operator-framework/deppy/pkg/deppy"
 	"github.com/operator-framework/deppy/pkg/deppy/input"
-	"k8s.io/apimachinery/pkg/util/sets"
 
 	"github.com/operator-framework/operator-controller/internal/catalogmetadata"
 	olmvariables "github.com/operator-framework/operator-controller/internal/resolution/variables"
 )
 
-var _ input.VariableSource = &CRDUniquenessConstraintsVariableSource{}
+// MakeBundleUniquenessVariables produces variables that constrain
+// the solution to at most 1 bundle per package.
+// These variables guarantee that no two versions of
+// the same package are running at the same time.
+func MakeBundleUniquenessVariables(bundleVariables []*olmvariables.BundleVariable) []*olmvariables.BundleUniquenessVariable {
+	result := []*olmvariables.BundleUniquenessVariable{}
+
+	bundleIDs := sets.Set[deppy.Identifier]{}
+	packageOrder := []string{}
+	bundleOrder := map[string][]deppy.Identifier{}
+	for _, bundleVariable := range bundleVariables {
+		bundles := make([]*catalogmetadata.Bundle, 0, 1+len(bundleVariable.Dependencies()))
+		bundles = append(bundles, bundleVariable.Bundle())
+		bundles = append(bundles, bundleVariable.Dependencies()...)
+		for _, bundle := range bundles {
+			id := olmvariables.BundleVariableID(bundle)
+			// get bundleID package and update map
+			packageName := bundle.Package
+
+			if _, ok := bundleOrder[packageName]; !ok {
+				packageOrder = append(packageOrder, packageName)
+			}
+
+			if !bundleIDs.Has(id) {
+				bundleIDs.Insert(id)
+				bundleOrder[packageName] = append(bundleOrder[packageName], id)
+			}
+		}
+	}
+
+	// create global constraint variables
+	for _, packageName := range packageOrder {
+		varID := deppy.IdentifierFromString(fmt.Sprintf("%s package uniqueness", packageName))
+		result = append(result, olmvariables.NewBundleUniquenessVariable(varID, bundleOrder[packageName]...))
+	}
+
+	return result
+}
 
 // CRDUniquenessConstraintsVariableSource produces variables that constraint the solution to
 // 1. at most 1 bundle per package
@@ -41,39 +79,17 @@ func (g *CRDUniquenessConstraintsVariableSource) GetVariables(ctx context.Contex
 		return nil, err
 	}
 
-	// todo(perdasilva): better handle cases where a provided gvk is not found
-	//                   not all packages will necessarily export a CRD
-
-	bundleIDs := sets.Set[deppy.Identifier]{}
-	packageOrder := []string{}
-	bundleOrder := map[string][]deppy.Identifier{}
+	bundleVariables := []*olmvariables.BundleVariable{}
 	for _, variable := range variables {
 		switch v := variable.(type) {
 		case *olmvariables.BundleVariable:
-			bundles := []*catalogmetadata.Bundle{v.Bundle()}
-			bundles = append(bundles, v.Dependencies()...)
-			for _, bundle := range bundles {
-				id := olmvariables.BundleVariableID(bundle)
-				// get bundleID package and update map
-				packageName := bundle.Package
-
-				if _, ok := bundleOrder[packageName]; !ok {
-					packageOrder = append(packageOrder, packageName)
-				}
-
-				if !bundleIDs.Has(id) {
-					bundleIDs.Insert(id)
-					bundleOrder[packageName] = append(bundleOrder[packageName], id)
-				}
-			}
+			bundleVariables = append(bundleVariables, v)
 		}
 	}
 
-	// create global constraint variables
-	for _, packageName := range packageOrder {
-		varID := deppy.IdentifierFromString(fmt.Sprintf("%s package uniqueness", packageName))
-		variables = append(variables, olmvariables.NewBundleUniquenessVariable(varID, bundleOrder[packageName]...))
+	bundleUniqueness := MakeBundleUniquenessVariables(bundleVariables)
+	for _, v := range bundleUniqueness {
+		variables = append(variables, v)
 	}
-
 	return variables, nil
 }
