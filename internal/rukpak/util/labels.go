6efeb48597f45a8f8124e45d138bab0eaafb42bc
Todd Short <tshort@redhat.com>
2024-04-18 10:38:40 -0400

    fixup! Update owner keys
    Signed-off-by: Todd Short <tshort@redhat.com>

--- internal/rukpak/util/labels.go
+++ internal/rukpak/util/labels.go
@@ -3,7 +3,6 @@ package util
 const (
 	OwnerKindKey     = "olm.operatorframework.io/owner-kind"
 	OwnerNameKey     = "olm.operatorframework.io/owner-name"
-	CatalogIdKey     = "olm.operatorframework.io/catalog-id"
 	PackageNameKey   = "olm.operatorframework.io/package-name"
 	BundleNameKey    = "olm.operatorframework.io/bundle-name"
 	BundleVersionKey = "olm.operatorframework.io/bundle-version"
