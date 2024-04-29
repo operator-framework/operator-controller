4785ca6bc94e41073446baaaa86d686b259edd80
Todd Short <tshort@redhat.com>
2024-04-29 14:08:46 -0400

    Helm POC rebase-a-thon

--- scripts/install.tpl.sh
+++ scripts/install.tpl.sh
@@ -11,19 +11,15 @@ fi
 
 catalogd_version=$CATALOGD_VERSION
 cert_mgr_version=$CERT_MGR_VERSION
-rukpak_version=$RUKPAK_VERSION
 
-if [[ -z "$catalogd_version" || -z "$cert_mgr_version" || -z "$rukpak_version" ]]; then
+if [[ -z "$catalogd_version" || -z "$cert_mgr_version" ]]; then
     err="Error: Missing component version(s) for: "
     if [[ -z "$catalogd_version" ]]; then
         err+="catalogd "
     fi 
     if [[ -z "$cert_mgr_version" ]]; then
         err+="cert-manager "
-    fi 
-    if [[ -z "$rukpak_version" ]]; then
-        err+="rukpak "
-    fi 
+    fi
     echo "$err"
     exit 1
 fi
@@ -38,11 +34,6 @@ function kubectl_wait() {
 
 kubectl apply -f "https://github.com/cert-manager/cert-manager/releases/download/${cert_mgr_version}/cert-manager.yaml"
 kubectl_wait "cert-manager" "deployment/cert-manager-webhook" "60s"
-
-kubectl apply -f "https://github.com/operator-framework/rukpak/releases/download/${rukpak_version}/rukpak.yaml"
-kubectl_wait "rukpak-system" "deployment/core" "60s"
-kubectl_wait "rukpak-system" "deployment/helm-provisioner" "60s"
-kubectl_wait "rukpak-system" "deployment/rukpak-webhooks" "60s"
 
 kubectl apply -f "https://github.com/operator-framework/catalogd/releases/download/${catalogd_version}/catalogd.yaml"
 kubectl_wait "catalogd-system" "deployment/catalogd-controller-manager" "60s"
