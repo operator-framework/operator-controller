8ae49f5cba8a94e3cf62b026cb7727c214eaeb71
Edmund Ochieng <ochienged@gmail.com>
2024-07-25 11:52:39 -0500

    tests: Test the unpack feature
    Signed-off-by: Edmund Ochieng <ochienged@gmail.com>

---
+++ internal/rukpak/source/image_registry_test.go
@@ -0,0 +1,40 @@
+package source_test
+
+import (
+	"context"
+	"testing"
+
+	"github.com/operator-framework/operator-controller/internal/rukpak/source"
+	ctrl "sigs.k8s.io/controller-runtime"
+	"sigs.k8s.io/controller-runtime/pkg/log"
+	"sigs.k8s.io/controller-runtime/pkg/log/zap"
+)
+
+func BenchmarkUnpack(b *testing.B) {
+	b.StopTimer()
+	cacheDir := b.TempDir()
+	mgr, _ := ctrl.NewManager(ctrl.GetConfigOrDie(), ctrl.Options{})
+	unpacker, _ := source.NewDefaultUnpacker(mgr, "default", cacheDir)
+
+	logger := zap.New(
+		zap.UseFlagOptions(
+			&zap.Options{
+				Development: true,
+			},
+		),
+	)
+
+	ctx := log.IntoContext(context.Background(), logger)
+
+	bundleSource := &source.BundleSource{
+		Type: source.SourceTypeImage,
+		Image: &source.ImageSource{
+			Ref: "quay.io/eochieng/litmus-edge-operator-bundle@sha256:104b294fa1f4c2e45aa0eac32437a51de32dce0eff923eced44a0dddcb7f363f",
+		},
+	}
+	b.StartTimer()
+
+	for i := 0; i < b.N; i++ {
+		_, _ = unpacker.Unpack(ctx, bundleSource)
+	}
+}
