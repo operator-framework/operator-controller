c653a71356a81bfa4b19c9fe9bfe19807b619f37
Mikalai Radchuk <mradchuk@redhat.com>
2024-09-02 17:47:29 +0200

    Remove catalogmetadata resource from RBAC
    This resource no longer exists in OLMv1
    
    Signed-off-by: Mikalai Radchuk <mradchuk@redhat.com>

--- internal/controllers/clusterextension_controller.go
+++ internal/controllers/clusterextension_controller.go
@@ -86,7 +86,6 @@ type InstalledBundleGetter interface {
 //+kubebuilder:rbac:groups=apiextensions.k8s.io,resources=customresourcedefinitions,verbs=get
 
 //+kubebuilder:rbac:groups=olm.operatorframework.io,resources=clustercatalogs,verbs=list;watch
-//+kubebuilder:rbac:groups=olm.operatorframework.io,resources=catalogmetadata,verbs=list;watch
 
 // The operator controller needs to watch all the bundle objects and reconcile accordingly. Though not ideal, but these permissions are required.
 // This has been taken from rukpak, and an issue was created before to discuss it: https://github.com/operator-framework/rukpak/issues/800.
