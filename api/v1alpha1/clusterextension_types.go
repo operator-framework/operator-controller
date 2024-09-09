0741d3610ded5878c89021fbfef1762e589be764
Jordan Keister <jordan@nimblewidget.com>
2024-09-09 15:38:38 -0500

    ⚠️ types conversions from review (#1223)
    * convert to non-pointer types in spec
    
    Signed-off-by: Jordan Keister <jordan@nimblewidget.com>
    
    * review resolutions
    
    Signed-off-by: Jordan Keister <jordan@nimblewidget.com>
    
    ---------
    
    Signed-off-by: Jordan Keister <jordan@nimblewidget.com>

--- api/v1alpha1/clusterextension_types.go
+++ api/v1alpha1/clusterextension_types.go
@@ -377,7 +377,8 @@ type ServiceAccountReference struct {
 	Name string `json:"name"`
 }
 
-// PreflightConfig holds the configuration for the preflight checks.
+// PreflightConfig holds the configuration for the preflight checks.  If used, at least one preflight check must be non-nil.
+// +kubebuilder:validation:XValidation:rule="has(self.crdUpgradeSafety)",message="at least one of [crdUpgradeSafety] are required when preflight is specified"
 type PreflightConfig struct {
 	// crdUpgradeSafety is used to configure the CRD Upgrade Safety pre-flight
 	// checks that run prior to upgrades of installed content.
