cc6f1c444937d0a64c66ef7532cb1ade8fa71224
Per Goncalves da Silva <pegoncal@redhat.com>
2025-02-06 17:38:05 +0100

    Get extension watch namespace from annotation
    Signed-off-by: Per Goncalves da Silva <pegoncal@redhat.com>

---
+++ internal/operator-controller/applier/watchnamespace_test.go
@@ -0,0 +1,83 @@
+package applier_test
+
+import (
+	"github.com/operator-framework/operator-controller/internal/operator-controller/applier"
+	"testing"
+
+	"github.com/stretchr/testify/require"
+	corev1 "k8s.io/api/core/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	featuregatetesting "k8s.io/component-base/featuregate/testing"
+
+	v1 "github.com/operator-framework/operator-controller/api/v1"
+	"github.com/operator-framework/operator-controller/internal/operator-controller/features"
+)
+
+func TestGetWatchNamespace(t *testing.T) {
+	featuregatetesting.SetFeatureGateDuringTest(t, features.OperatorControllerFeatureGate, features.SingleOwnNamespaceInstallSupport, true)
+
+	for _, tt := range []struct {
+		name        string
+		want        string
+		csv         *v1.ClusterExtension
+		expectError bool
+	}{
+		{
+			name: "cluster extension does not have watch namespace annotation",
+			want: corev1.NamespaceAll,
+			csv: &v1.ClusterExtension{
+				ObjectMeta: metav1.ObjectMeta{
+					Name:        "extension",
+					Annotations: nil,
+				},
+				Spec: v1.ClusterExtensionSpec{},
+			},
+			expectError: false,
+		}, {
+			name: "cluster extension has valid namespace annotation",
+			want: "watch-namespace",
+			csv: &v1.ClusterExtension{
+				ObjectMeta: metav1.ObjectMeta{
+					Name: "extension",
+					Annotations: map[string]string{
+						"olm.operatorframework.io/watch-namespace": "watch-namespace",
+					},
+				},
+				Spec: v1.ClusterExtensionSpec{},
+			},
+			expectError: false,
+		}, {
+			name: "cluster extension has invalid namespace annotation: multiple watch namespaces",
+			want: "",
+			csv: &v1.ClusterExtension{
+				ObjectMeta: metav1.ObjectMeta{
+					Name: "extension",
+					Annotations: map[string]string{
+						"olm.operatorframework.io/watch-namespace": "watch-namespace,watch-namespace2,watch-namespace3",
+					},
+				},
+				Spec: v1.ClusterExtensionSpec{},
+			},
+			expectError: true,
+		}, {
+			name: "cluster extension has invalid namespace annotation: invalid name",
+			want: "",
+			csv: &v1.ClusterExtension{
+				ObjectMeta: metav1.ObjectMeta{
+					Name: "extension",
+					Annotations: map[string]string{
+						"olm.operatorframework.io/watch-namespace": "watch-namespace-",
+					},
+				},
+				Spec: v1.ClusterExtensionSpec{},
+			},
+			expectError: true,
+		},
+	} {
+		t.Run(tt.name, func(t *testing.T) {
+			got, err := applier.GetWatchNamespace(tt.csv)
+			require.Equal(t, tt.want, got)
+			require.Equal(t, tt.expectError, err != nil)
+		})
+	}
+}
