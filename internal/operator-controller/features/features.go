f26be512e94859a5f90f0c6f5446f43294f3dc7e
Per Goncalves da Silva <pegoncal@redhat.com>
2025-02-06 17:37:32 +0100

    Add feature gate
    Signed-off-by: Per Goncalves da Silva <pegoncal@redhat.com>

--- internal/operator-controller/features/features.go
+++ internal/operator-controller/features/features.go
@@ -8,13 +8,23 @@ import (
 const (
 	// Add new feature gates constants (strings)
 	// Ex: SomeFeature featuregate.Feature = "SomeFeature"
-	PreflightPermissions featuregate.Feature = "PreflightPermissions"
+	PreflightPermissions             featuregate.Feature = "PreflightPermissions"
+	SingleOwnNamespaceInstallSupport featuregate.Feature = "SingleOwnNamespaceInstallSupport"
 )
 
 var operatorControllerFeatureGates = map[featuregate.Feature]featuregate.FeatureSpec{
 	// Add new feature gate definitions
 	// Ex: SomeFeature: {...}
 	PreflightPermissions: {
+		Default:       false,
+		PreRelease:    featuregate.Alpha,
+		LockToDefault: false,
+	},
+
+	// SingleOwnNamespaceInstallSupport enables support for installing
+	// registry+v1 cluster extensions with single or own namespaces modes
+	// i.e. with a single watch namespace.
+	SingleOwnNamespaceInstallSupport: {
 		Default:       false,
 		PreRelease:    featuregate.Alpha,
 		LockToDefault: false,
