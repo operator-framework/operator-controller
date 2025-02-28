427d0e6f3b25b7d7c87a66a6e04dbb62ecfb8fd2
Jordan Keister <jordan@nimblewidget.com>
2025-02-28 13:53:02 -0600

    add metas endpoint demo script
    Signed-off-by: Jordan Keister <jordan@nimblewidget.com>

--- hack/demo/catalogd-metas-demo-script.sh
+++ hack/demo/catalogd-metas-demo-script.sh
@@ -4,13 +4,13 @@
 #
 trap 'trap - SIGTERM && kill -- -"$$"' SIGINT SIGTERM EXIT
 
-kind delete cluster
-kind create cluster
-kubectl cluster-info --context kind-kind
-sleep 10
-
-# use the install script from the latest github release
-curl -L -s https://github.com/operator-framework/operator-controller/releases/latest/download/install.sh | bash
+## until we cut a release containing the new API, we need to deploy local artifacts
+make kind-clean
+make run
+#
+## use the install script from the latest github release
+#curl -L -s https://github.com/operator-framework/operator-controller/releases/latest/download/install.sh | bash
+#
 
 # inspect crds (clustercatalog)
 kubectl get crds -A
@@ -26,19 +26,19 @@ kubectl patch -n olmv1-system deploy/catalogd-controller-manager --type='json' -
 kubectl rollout status -n olmv1-system deploy/catalogd-controller-manager
 # ... checking clustercatalog is serving
 kubectl wait --for=condition=Serving clustercatalog/operatorhubio --timeout=60s
-# ... checking clustercatalog is finished unpacking
-kubectl wait --for=condition=Progressing=False clustercatalog/operatorhubio --timeout=60s
+# ... checking clustercatalog is finished unpacking (progressing gone back to true)
+kubectl wait --for=condition=Progressing=True clustercatalog/operatorhubio --timeout=60s
 
+sleep 30
 
 # port forward the catalogd-service service to interact with the HTTP server serving catalog contents
 (kubectl -n olmv1-system port-forward svc/catalogd-service 8081:443)&
 
-sleep 3
 
 # check what 'packages' are available in this catalog
-curl -vvv -k 'https://localhost:8081/catalogs/operatorhubio/api/v1/metas?schema=olm.package' | jq -s '.[] | .name'
+curl -f -k 'https://localhost:8081/catalogs/operatorhubio/api/v1/metas?schema=olm.package' | jq -s '.[] | .name'
 # check what channels are included in the wavefront package
-curl -vvv -k 'https://localhost:8081/catalogs/operatorhubio/api/v1/metas?schema=olm.channel&package=wavefront' | jq -s '.[] | .name'
+curl -f -k 'https://localhost:8081/catalogs/operatorhubio/api/v1/metas?schema=olm.channel&package=wavefront' | jq -s '.[] |.name'
 # check what bundles are included in the wavefront package
-curl -vvv -k 'https://localhost:8081/catalogs/operatorhubio/api/v1/metas?schema=olm.bundle&package=wavefront' | jq -s '.[] | .name'
+curl -f -k 'https://localhost:8081/catalogs/operatorhubio/api/v1/metas?schema=olm.bundle&package=wavefront' | jq -s '.[] |.name'
 
