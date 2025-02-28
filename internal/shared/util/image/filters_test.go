83cf868848b7b7926c143116f434670b937aeffe
Joe Lanford <joe.lanford@gmail.com>
2025-02-27 21:43:44 -0500

    util/image: unset PAXRecords and Xattrs when applying files
    Signed-off-by: Joe Lanford <joe.lanford@gmail.com>

--- internal/shared/util/image/filters_test.go
+++ internal/shared/util/image/filters_test.go
@@ -2,10 +2,38 @@ package image
 
 import (
 	"archive/tar"
+	"os"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+	"k8s.io/apimachinery/pkg/util/rand"
 )
+
+func TestForceOwnershipRWX(t *testing.T) {
+	h := tar.Header{
+		Name: "foo/bar",
+		Mode: 0000,
+		Uid:  rand.Int(),
+		Gid:  rand.Int(),
+		Xattrs: map[string]string{ //nolint:staticcheck
+			"foo": "bar",
+		},
+		PAXRecords: map[string]string{
+			"fizz": "buzz",
+		},
+	}
+	ok, err := forceOwnershipRWX()(&h)
+	require.NoError(t, err)
+	assert.True(t, ok)
+
+	assert.Equal(t, "foo/bar", h.Name)
+	assert.Equal(t, int64(0700), h.Mode)
+	assert.Equal(t, os.Getuid(), h.Uid)
+	assert.Equal(t, os.Getgid(), h.Gid)
+	assert.Nil(t, h.PAXRecords)
+	assert.Nil(t, h.Xattrs) //nolint:staticcheck
+}
 
 func TestOnlyPath(t *testing.T) {
 	type testCase struct {
