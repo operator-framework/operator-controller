6a6c2234be0d2d98e446e6beb1c0db810dcf3509
Todd Short <tshort@redhat.com>
2025-05-20 13:39:49 -0400

    TMP: crd-generator-work
    Signed-off-by: Todd Short <tshort@redhat.com>

---
+++ hack/tools/update-crds.sh
@@ -0,0 +1,42 @@
+#!/usr/bin/env bash
+# This uses a custom CRD generator to create "standard" and "experimental" CRDs
+
+# The names of the generated CRDs
+CE="olm.operatorframework.io_clusterextensions.yaml"
+CC="olm.operatorframework.io_clustercatalogs.yaml"
+
+# order for modules and crds must match
+# each item in crds must be unique, and should be associated with a module
+modules=("operator-controller" "catalogd")
+crds=("${CE}" "${CC}")
+
+# Channels must much those in the generator
+channels=("standard" "experimental")
+
+# Create the temp output directories
+CRD_TMP=$(mktemp -d)
+for c in ${channels[@]}; do
+    mkdir -p ${CRD_TMP}/${c}
+done
+
+CONTROLLER_TOOLS_VER=$(go list -m sigs.k8s.io/controller-tools | awk '{print $2}')
+
+# Generate the CRDs
+go run ./pkg/crd-generator ${CRD_TMP} ${CONTROLLER_TOOLS_VER}
+
+# Create the destination directories for each base/channel combo
+for c in ${channels[@]}; do
+    for b in ${modules[@]}; do
+        mkdir -p config/base/${b}/crd/${c}
+    done
+done
+
+# Copy the generated files
+for b in ${!modules[@]}; do
+    for c in ${channels[@]}; do
+        cp ${CRD_TMP}/${c}/${crds[${b}]} config/base/${modules[${b}]}/crd/${c}
+    done
+done
+
+# Clean up the temp output directories
+rm -rf ${CRD_TMP}
