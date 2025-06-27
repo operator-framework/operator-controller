07a1b51d4f3c9491d7e99103dd232b6019f13d36
Todd Short <tshort@redhat.com>
2025-06-27 14:38:06 -0400

    Add experimental e2e tests
    This adds `manifests/experimental-e2e.yaml`, and uses it to run the
    experimental-e2e.
    
    The experimental-e2e is run via `make test-experimental-e2e` and a
    CI job is added to run it.
    
    A "no-op" experimental-e2e test has been added.
    
    This is part of the feature-gated API functionality.
    
    Signed-off-by: Todd Short <tshort@redhat.com>

---
+++ test/experimental-e2e/experimental_e2e_test.go
@@ -0,0 +1,34 @@
+package experimental_e2e
+
+import (
+	"os"
+	"testing"
+
+	apiextensionsv1 "k8s.io/apiextensions-apiserver/pkg/apis/apiextensions/v1"
+	utilruntime "k8s.io/apimachinery/pkg/util/runtime"
+	"k8s.io/client-go/rest"
+	ctrl "sigs.k8s.io/controller-runtime"
+	"sigs.k8s.io/controller-runtime/pkg/client"
+
+	"github.com/operator-framework/operator-controller/internal/operator-controller/scheme"
+)
+
+var (
+	cfg *rest.Config
+	c   client.Client
+)
+
+func TestMain(m *testing.M) {
+	cfg = ctrl.GetConfigOrDie()
+
+	var err error
+	utilruntime.Must(apiextensionsv1.AddToScheme(scheme.Scheme))
+	c, err = client.New(cfg, client.Options{Scheme: scheme.Scheme})
+	utilruntime.Must(err)
+
+	os.Exit(m.Run())
+}
+
+func TestNoop(t *testing.T) {
+	t.Log("Running experimental-e2e tests")
+}
