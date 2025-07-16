a0717aaee9663dcf8d2283349544365262beb096
Todd Short <tmshort@users.noreply.github.com>
2025-07-16 09:40:19 -0400

    Remove use of namespace in kustomize (#2095)
    The use of the namespace parameter is kustomization files is very tricky.
    In one particular instance, we have an ordering issue with the cert-manager
    CA component. If not ordered correctly in the set of kustomization files,
    the CA component namespace will be overwritten by prior namespace
    directives.
    
    This eliminates that edge case, and makes the kustomization more robust.
    
    Downstream uses a different overlay, so there's no issue there.
    
    Also, add `-n` option to the install script to allow users to easily
    change the namespace that they install OLMv1 into.
    
    Note that the manifests don't change; so this keep everything as-is.
    
    Signed-off-by: Todd Short <tshort@redhat.com>

--- scripts/install.tpl.sh
+++ scripts/install.tpl.sh
@@ -3,6 +3,39 @@ set -euo pipefail
 IFS=$'\n\t'
 
 olmv1_manifest=$MANIFEST
+olmv1_namespace=olmv1-system
+
+usage() {
+    cmd=$(basename $0)
+    cat <<EOF
+NAME
+    ${cmd} - install OLMv1 into a cluster
+
+SYNOPSIS
+    ${cmd} [-n <namespace>] [-h]
+
+DESCRIPTION
+    Installs OLMv1 in the provided <namespace> with cert-manager.
+    A kubernetes configuration must already be present.
+
+    -n <namespace>
+        install OLMv1 in the given <namespace>. Defaults to olmv1-system.
+
+    -h
+        help (this text)
+EOF
+    exit 0
+}
+
+
+while getopts n:h opt; do
+    case ${opt} in
+        n) olmv1_namespace=${OPTARG} ;;
+        h) usage ;;
+        *) echo "Unknown option" >&2
+           exit 1
+    esac
+done
 
 if [[ -z "$olmv1_manifest" ]]; then
     echo "Error: Missing required MANIFEST variable"
@@ -69,11 +102,16 @@ kubectl_wait "cert-manager" "deployment/cert-manager" "60s"
 kubectl_wait_for_query "mutatingwebhookconfigurations/cert-manager-webhook" '{.webhooks[0].clientConfig.caBundle}' 60 5
 kubectl_wait_for_query "validatingwebhookconfigurations/cert-manager-webhook" '{.webhooks[0].clientConfig.caBundle}' 60 5
 
-kubectl apply -f "${olmv1_manifest}"
+# Change the file into a file:// url
+if [ -f "${olmv1_manifest}" ]; then
+    olmv1_manifest=file://localhost$(realpath ${olmv1_manifest})
+fi
+
+curl -L -s "${olmv1_manifest}" | sed "s/olmv1-system/${olmv1_namespace}/g" | kubectl apply -f -
 # Wait for the rollout, and then wait for the deployment to be Available
-kubectl_wait_rollout "olmv1-system" "deployment/catalogd-controller-manager" "60s"
-kubectl_wait "olmv1-system" "deployment/catalogd-controller-manager" "60s"
-kubectl_wait "olmv1-system" "deployment/operator-controller-controller-manager" "60s"
+kubectl_wait_rollout "${olmv1_namespace}" "deployment/catalogd-controller-manager" "60s"
+kubectl_wait "${olmv1_namespace}" "deployment/catalogd-controller-manager" "60s"
+kubectl_wait "${olmv1_namespace}" "deployment/operator-controller-controller-manager" "60s"
 
 if [[ "${install_default_catalogs}" != "false" ]]; then
     kubectl apply -f "${default_catalogs_manifest}"
