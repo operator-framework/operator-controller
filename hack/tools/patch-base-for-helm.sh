e8414dadd8888aa61a2eb58a9820de977e92208b
Todd Short <tshort@redhat.com>
2025-07-29 11:52:21 -0400

    WIP: Remove generation of rbac and webhooks
    Signed-off-by: Todd Short <tshort@redhat.com>

--- hack/tools/patch-base-for-helm.sh
+++ hack/tools/patch-base-for-helm.sh
@@ -7,53 +7,11 @@
 # YAML once helm templating has been added.
 
 # Patch catalogd rbac
-catalogd_rbac_filelist=(
-    helm/olmv1/base/catalogd/rbac/experimental/*.yaml
-    helm/olmv1/base/catalogd/rbac/standard/*.yaml
-)
-for f in "${catalogd_rbac_filelist[@]}"; do
-    yq -i '.metadata.labels["app.kubernetes.io/name"] = "catalogd"' "${f}"
-    yq -i 'with(.; select(.kind == "Role") | .rules += { "replaceMe": "catalogd-role-rules"})' "${f}"
-    yq -i 'with(.; select(.kind == "ClusterRole") | .rules += { "replaceMe": "catalogd-cluster-role-rules"})' "${f}"
-done
-
-# Patch operator-controller rbac
-operator_controller_rbac_filelist=(
-    helm/olmv1/base/operator-controller/rbac/experimental/*.yaml
-    helm/olmv1/base/operator-controller/rbac/standard/*.yaml
-)
-for f in "${operator_controller_rbac_filelist[@]}"; do
-    yq -i '.metadata.labels["app.kubernetes.io/name"] = "operator-controller"' "${f}"
-    yq -i 'with(.; select(.kind == "Role") | .rules += { "replaceMe": "operator-controller-role-rules"})' "${f}"
-    yq -i 'with(.; select(.kind == "ClusterRole") | .rules += { "replaceMe": "operator-controller-cluster-role-rules"})' "${f}"
-done
-
-# Patch catalogd webhook
-catalogd_webhook_filelist=(
-    helm/olmv1/base/catalogd/webhook/experimental/*.yaml
-    helm/olmv1/base/catalogd/webhook/standard/*.yaml
-)
-for f in "${catalogd_webhook_filelist[@]}"; do
-    yq -i '.metadata.labels["app.kubernetes.io/name"] = "catalogd"' "${f}"
-    yq -i '.metadata.name = "catalogd-mutating-webhook-configuration"' "${f}"
-    yq -i '.metadata.annotations["catalogd-webhook-annotations"] = "replaceMe"' "${f}"
-    yq -i '.webhooks[0].clientConfig.service.namespace = "olmv1-system"' "${f}"
-    yq -i '.webhooks[0].clientConfig.service.name = "catalogd-service"' "${f}"
-    yq -i '.webhooks[0].clientConfig.service.port = 9443' "${f}"
-    yq -i '.webhooks[0].matchConditions[0].name = "MissingOrIncorrectMetadataNameLabel"' "${f}"
-    yq -i '.webhooks[0].matchConditions[0].expression = "\"name\" in object.metadata && (!has(object.metadata.labels) || !(\"olm.operatorframework.io/metadata.name\" in object.metadata.labels) || object.metadata.labels[\"olm.operatorframework.io/metadata.name\"] != object.metadata.name)"' "${f}"
-done
 
 # Patch everything generically
 filelist=(
-    helm/olmv1/base/catalogd/rbac/experimental/*.yaml
-    helm/olmv1/base/catalogd/rbac/standard/*.yaml
     helm/olmv1/base/catalogd/crd/experimental/*.yaml
     helm/olmv1/base/catalogd/crd/standard/*.yaml
-    helm/olmv1/base/catalogd/webhook/experimental/*.yaml
-    helm/olmv1/base/catalogd/webhook/standard/*.yaml
-    helm/olmv1/base/operator-controller/rbac/experimental/*.yaml
-    helm/olmv1/base/operator-controller/rbac/standard/*.yaml
     helm/olmv1/base/operator-controller/crd/experimental/*.yaml
     helm/olmv1/base/operator-controller/crd/standard/*.yaml
 )
@@ -64,13 +22,7 @@ for f in "${filelist[@]}"; do
     yq -i '.metadata.labels.replaceMe = "labels"' "${f}"
     # Replace with helm template - must be done last or yq will complain about the file format
     sed -i.bak 's/replaceMe: annotations/{{- include "olmv1.annotations" . | nindent 4 }}/g' "${f}"
-    sed -i.bak 's/catalogd-webhook-annotations: replaceMe/{{- include "olmv1.catalogd.webhook.annotations" . | nindent 4 }}/g' "${f}"
     sed -i.bak 's/replaceMe: labels/{{- include "olmv1.labels" . | nindent 4 }}/g' "${f}"
-    sed -i.bak 's/olmv1-system/{{ .Values.namespaces.olmv1.name }}/g' "${f}"
-    sed -i.bak 's/- replaceMe: catalogd-role-rules/{{- include "olmv1.catalogd.role.rules" . | nindent 2 }}/g' "${f}"
-    sed -i.bak 's/- replaceMe: catalogd-cluster-role-rules/{{- include "olmv1.catalogd.clusterRole.rules" . | nindent 2 }}/g' "${f}"
-    sed -i.bak 's/- replaceMe: operator-controller-role-rules/{{- include "olmv1.operatorController.role.rules" . | nindent 2 }}/g' "${f}"
-    sed -i.bak 's/- replaceMe: operator-controller-cluster-role-rules/{{- include "olmv1.operatorController.clusterRole.rules" . | nindent 2 }}/g' "${f}"
     # Delete sed's backup file
     rm -f "${f}.bak"
 done
