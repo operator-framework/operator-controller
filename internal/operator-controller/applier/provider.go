3a28e97bfb0f31b7b0104bb536ed1129d8f550f9
Per G. da Silva <pegoncal@redhat.com>
2025-10-20 15:56:26 -0700

    Update manifest provider to use config unmarshal and remove getWatchNamespace
    Signed-off-by: Per G. da Silva <pegoncal@redhat.com>

--- internal/operator-controller/applier/provider.go
+++ internal/operator-controller/applier/provider.go
@@ -3,20 +3,17 @@ package applier
 import (
 	"crypto/sha256"
 	"encoding/json"
-	"errors"
 	"fmt"
 	"io/fs"
-	"strings"
 
 	"helm.sh/helm/v3/pkg/chart"
 	"k8s.io/apimachinery/pkg/util/sets"
-	"k8s.io/apimachinery/pkg/util/validation"
 	"sigs.k8s.io/controller-runtime/pkg/client"
-	"sigs.k8s.io/yaml"
 
 	"github.com/operator-framework/api/pkg/operators/v1alpha1"
 
 	ocv1 "github.com/operator-framework/operator-controller/api/v1"
+	"github.com/operator-framework/operator-controller/internal/operator-controller/rukpak/bundle"
 	"github.com/operator-framework/operator-controller/internal/operator-controller/rukpak/bundle/source"
 	"github.com/operator-framework/operator-controller/internal/operator-controller/rukpak/render"
 )
@@ -34,9 +31,6 @@ type RegistryV1ManifestProvider struct {
 	CertificateProvider         render.CertificateProvider
 	IsWebhookSupportEnabled     bool
 	IsSingleOwnNamespaceEnabled bool
-}
-type registryV1Config struct {
-	WatchNamespace string `json:"watchNamespace"`
 }
 
 func (r *RegistryV1ManifestProvider) Get(bundleFS fs.FS, ext *ocv1.ClusterExtension) ([]client.Object, error) {
@@ -74,44 +68,18 @@ func (r *RegistryV1ManifestProvider) Get(bundleFS fs.FS, ext *ocv1.ClusterExtens
 		render.WithCertificateProvider(r.CertificateProvider),
 	}
 
-	watchNamespace, err := r.getWatchNamespace(ext)
-	if err != nil {
-		return nil, fmt.Errorf("invalid bundle configuration: %w", err)
-	}
+	if r.IsSingleOwnNamespaceEnabled && ext.Spec.Config != nil && ext.Spec.Config.ConfigType == ocv1.ClusterExtensionConfigTypeInline {
+		bundleConfig, err := bundle.UnmarshallConfig(ext.Spec.Config.Inline.Raw, &rv1, ext.Spec.Namespace)
+		if err != nil {
+			return nil, fmt.Errorf("invalid bundle configuration: %w", err)
+		}
 
-	if watchNamespace != "" {
-		opts = append(opts, render.WithTargetNamespaces(watchNamespace))
+		if bundleConfig != nil && bundleConfig.WatchNamespace != "" {
+			opts = append(opts, render.WithTargetNamespaces(bundleConfig.WatchNamespace))
+		}
 	}
 
 	return r.BundleRenderer.Render(rv1, ext.Spec.Namespace, opts...)
-}
-
-// getWatchNamespace determines the watch namespace the ClusterExtension should use based on the
-// configuration in .spec.config.Inline. Only active if SingleOwnNamespace support is enabled.
-func (r *RegistryV1ManifestProvider) getWatchNamespace(ext *ocv1.ClusterExtension) (string, error) {
-	if !r.IsSingleOwnNamespaceEnabled {
-		return "", nil
-	}
-
-	var watchNamespace string
-	if ext.Spec.Config != nil && ext.Spec.Config.Inline != nil {
-		cfg := &registryV1Config{}
-		// Using k8s.io/yaml package as that is able to handle both json and yaml
-		// In most cases, at this point we should have a valid JSON/YAML object in the byte slice and failures will
-		// be related to object structure (e.g. additional fields).
-		if err := yaml.UnmarshalStrict(ext.Spec.Config.Inline.Raw, cfg); err != nil {
-			return "", fmt.Errorf("error unmarshalling registry+v1 configuration: %w", formatUnmarshallError(err))
-		}
-		watchNamespace = cfg.WatchNamespace
-	} else {
-		return "", nil
-	}
-
-	if errs := validation.IsDNS1123Subdomain(watchNamespace); len(errs) > 0 {
-		return "", fmt.Errorf("invalid watch namespace '%s': namespace must consist of lower case alphanumeric characters, '-' or '.', and must start and end with an alphanumeric character", watchNamespace)
-	}
-
-	return watchNamespace, nil
 }
 
 // RegistryV1HelmChartProvider creates a Helm-Chart from a registry+v1 bundle and its associated ClusterExtension
@@ -159,28 +127,4 @@ func (r *RegistryV1HelmChartProvider) Get(bundleFS fs.FS, ext *ocv1.ClusterExten
 	}
 
 	return chrt, nil
-}
-
-func formatUnmarshallError(err error) error {
-	var unmarshalErr *json.UnmarshalTypeError
-	if errors.As(err, &unmarshalErr) {
-		if unmarshalErr.Field == "" {
-			return errors.New("input is not a valid JSON object")
-		} else {
-			return fmt.Errorf("invalid value type for field %q: expected %q but got %q", unmarshalErr.Field, unmarshalErr.Type.String(), unmarshalErr.Value)
-		}
-	}
-
-	// unwrap error until the core and process it
-	for {
-		unwrapped := errors.Unwrap(err)
-		if unwrapped == nil {
-			// usually the errors present in the form json: <message> or yaml: <message>
-			// we want to extract <message> if we can
-			errMessageComponents := strings.Split(err.Error(), ":")
-			coreErrMessage := strings.TrimSpace(errMessageComponents[len(errMessageComponents)-1])
-			return errors.New(coreErrMessage)
-		}
-		err = unwrapped
-	}
 }
