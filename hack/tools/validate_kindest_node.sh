03eb88435c3753f2101fdaeaddfa7867da7ef6ff
Todd Short <tmshort@users.noreply.github.com>
2025-11-19 09:42:01 -0500

    Use local cache to check kind version (#2346)
    Use the local cached source of kind to determine the k8s version it
    supports. It avoids hitting github if possible.
    
    Signed-off-by: Todd Short <tshort@redhat.com>

--- hack/tools/validate_kindest_node.sh
+++ hack/tools/validate_kindest_node.sh
@@ -4,8 +4,24 @@
 # Extract the version of kind, by removing the "${GOBIN}/kind-" prefix
 KIND=${KIND#${GOBIN}/kind-}
 
-# Get the version of the image
-KIND_VER=$(curl -L -s https://github.com/kubernetes-sigs/kind/raw/refs/tags/${KIND}/pkg/apis/config/defaults/image.go | grep -Eo 'v[0-9]+\.[0-9]+')
+GOMODCACHE=$(go env GOMODCACHE)
+
+REGEX='v[0-9]+\.[0-9]+'
+
+# Get the version of the image from the local kind build
+if [ -d "${GOMODCACHE}" ]; then
+    KIND_VER=$(grep -Eo "${REGEX}" ${GOMODCACHE}/sigs.k8s.io/kind@${KIND}/pkg/apis/config/defaults/image.go)
+fi
+
+# Get the version of the image from github
+if [ -z "${KIND_VER}" ]; then
+    KIND_VER=$(curl -L -s https://github.com/kubernetes-sigs/kind/raw/refs/tags/${KIND}/pkg/apis/config/defaults/image.go | grep -Eo "${REGEX}")
+fi
+
+if [ -z "${KIND_VER}" ]; then
+    echo "Unable to determine kindest/node version"
+    exit 1
+fi
 
 # Compare the versions
 if [ "${KIND_VER}" != "${K8S_VERSION}" ]; then
